3059460b1786b19334429097bcdadc66930bfe23
Somitatfi <somiyac.ext@teachforindia.org>
2025-09-12 13:24:14 +0530

    Add all microservices and client folder for GPay app

---
+++ services/email-service/src/test/java/com/gpay/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpay.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ services/user-service/src/test/java/com/gpay/user_service/GpayUserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpay.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpayUserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
